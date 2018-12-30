984fd118dee69c4f46bf51ba6fb210cb96dd892e
Peter Squicciarini <stripedpajamas273@gmail.com>
2018-12-29 21:15:22 -0500

    Implement ulid function

--- lib/ulid.ml
+++ lib/ulid.ml
@@ -1,18 +1,17 @@
 (*
-  ULID is [48 bits timestamp (6 bytes)] + [80 bits random (10 bytes)]
+  ULID is [48 bits timestamp] + [80 bits random]
   encoded in Crockford's base32
 *)
 (* Crockford's Base32 *)
 let encoding = [|'0';'1';'2';'3';'4';'5';'6';'7';'8';'9';'A';'B';'C';'D';'E';'F';'G';'H';'J';'K';'M';'N';'P';'Q';'R';'S';'T';'V';'W';'X';'Y';'Z';|];;
 let encoding_len = Array.length encoding;;
 let time_len = 10;;
+let random_len = 16;;
 Random.self_init ();;
 
 let random_char () =
   let idx = Random.int encoding_len in
     Array.get encoding idx
-
-let m = 0 mod 1;;
 
 let encode_time now len =
   let t = ref now in
@@ -25,4 +24,21 @@ let encode_time now len =
   let s = (Buffer.contents str) in
   String.init len (fun i -> s.[len - 1 - i])
 
+let encode_random len = 
+  let str = Buffer.create len in
+  for _ = len downto 1 do
+    Buffer.add_char str (random_char ())
+  done;
+  let s = (Buffer.contents str) in
+  String.init len (fun i -> s.[len - 1 - i])
+
 let increment_base_32 str = str
+
+let get_now () =
+  int_of_float (1000. *. (Unix.gettimeofday ()))
+
+let ulid ?(seed_time = (get_now ())) () =
+  let s = Buffer.create (time_len + encoding_len) in
+  Buffer.add_string s (encode_time seed_time time_len);
+  Buffer.add_string s (encode_random random_len);
+  Buffer.contents s
--- test/test.ml
+++ test/test.ml
@@ -17,8 +17,36 @@ let test_set_1 = [
 let encode_time_1 () =
   Alcotest.(check string) "encodes properly" "01ARYZ6S41" (Ulid.encode_time 1469918176385 10)
 
+let encode_time_2 () =
+  Alcotest.(check string) "changes length properly" "0001AS99AA60" (Ulid.encode_time 1470264322240 12)
+
+let encode_time_3 () =
+  Alcotest.(check string) "truncates length properly" "AS4Y1E11" (Ulid.encode_time 1470118279201 8)
+
 let test_set_2 = [
-  "encode time encodes properly", `Quick, encode_time_1;
+  "encode time encodes time", `Quick, encode_time_1;
+  "encode time changes length", `Quick, encode_time_2;
+  "encode time truncates length", `Quick, encode_time_3;
+]
+
+let encode_random_1 () =
+  Alcotest.(check int) "returns current length" 12 (String.length (Ulid.encode_random 12))
+
+let test_set_3 = [
+  "encode random returns correct length", `Quick, encode_random_1;
+]
+
+let ulid_1 () =
+  Alcotest.(check int) "returns current length" 26 (String.length (Ulid.ulid ()))
+
+let ulid_2 () =
+  let ulid = (Ulid.ulid ~seed_time:1469918176385 ()) in
+  let time = (String.sub ulid 0 10) in
+  Alcotest.(check string) "returns expected time encoded" "01ARYZ6S41" time
+
+let test_set_4 = [
+  "ulid returns correct length", `Quick, ulid_1;
+  "ulid correctly encodes time", `Quick, ulid_2;
 ]
 
 (* Run it *)
@@ -26,4 +54,6 @@ let () =
   Alcotest.run "ULID tests" [
     (* "base 32 increment", test_set; *)
     "encode time", test_set_2;
+    "encode random", test_set_3;
+    "ulid", test_set_4;
   ]
