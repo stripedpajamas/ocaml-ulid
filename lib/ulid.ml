9b4203150b17225aee9229f9fb93eee7d5603fa8
Peter Squicciarini <stripedpajamas273@gmail.com>
2018-12-29 20:28:24 -0500

    Encode time functionality

--- lib/ulid.ml
+++ lib/ulid.ml
@@ -2,5 +2,27 @@
   ULID is [48 bits timestamp (6 bytes)] + [80 bits random (10 bytes)]
   encoded in Crockford's base32
 *)
+(* Crockford's Base32 *)
+let encoding = [|'0';'1';'2';'3';'4';'5';'6';'7';'8';'9';'A';'B';'C';'D';'E';'F';'G';'H';'J';'K';'M';'N';'P';'Q';'R';'S';'T';'V';'W';'X';'Y';'Z';|];;
+let encoding_len = Array.length encoding;;
+let time_len = 10;;
+Random.self_init ();;
+
+let random_char () =
+  let idx = Random.int encoding_len in
+    Array.get encoding idx
+
+let m = 0 mod 1;;
+
+let encode_time now len =
+  let t = ref now in
+  let str = Buffer.create len in
+  for _ = len downto 1 do
+    let m = !t mod encoding_len in
+    Buffer.add_char str (Array.get encoding m);
+    t := (!t - m) / encoding_len;
+  done;
+  let s = (Buffer.contents str) in
+  String.init len (fun i -> s.[len - 1 - i])
 
 let increment_base_32 str = str
--- test/test.ml
+++ test/test.ml
@@ -8,14 +8,22 @@ let increment_base32_2 () =
 let increment_base32_3 () =
   Alcotest.(check string) "double increments correctly" "A1Z01" (Ulid.increment_base_32("A1YZZ"))
 
-let test_set = [
+let test_set_1 = [
   "base 32 increment increments correctly", `Quick, increment_base32_1;
   "base 32 increment carries correctly", `Quick, increment_base32_2;
   "base 32 increment double increments correctly", `Quick, increment_base32_3;
 ]
 
+let encode_time_1 () =
+  Alcotest.(check string) "encodes properly" "01ARYZ6S41" (Ulid.encode_time 1469918176385 10)
+
+let test_set_2 = [
+  "encode time encodes properly", `Quick, encode_time_1;
+]
+
 (* Run it *)
 let () =
   Alcotest.run "ULID tests" [
-    "base 32 increment", test_set;
+    (* "base 32 increment", test_set; *)
+    "encode time", test_set_2;
   ]
