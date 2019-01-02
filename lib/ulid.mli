afa14beb7da5a727d54d2d400b4c8941fa7abaca
Peter Squicciarini <stripedpajamas273@gmail.com>
2019-01-02 13:43:11 -0500

    ocamlformat

--- lib/ulid.mli
+++ lib/ulid.mli
@@ -1,16 +1,29 @@
-val encoding : char array
-val encoding_len : int
-val time_len : int
-val random_len : int
+(** ULID implementation.
+
+    ULIDs are Universally Unique Lexicographically Sortable Identifier
+    See the spec at https://github.com/ulid/spec
+    
+    {e Release %%VERSION%% - %%PKG_HOMEPAGE%%} *)
+
 val get_nocrypto_rng : unit -> int -> int
-val random_char : (int -> int) -> unit -> char
-val find_in_array : 'a array -> 'a -> int -> int
+(** A getter for the initialized nocrypto RNG in function form int -> int. *)
+
 val encode_time : int -> int -> string
+(** Encodes a UNIX timestamp in milliseconds into Base32. *)
+
 val encode_random : int -> (int -> int) -> string
-val replace_char_at : bytes -> int -> char -> bytes
+(** Encodes specified number of random ints into Base32 using specified PRNG. *)
+
 val increment_base_32 : string -> string
-val get_now : unit -> int
+(** Increments a Base32 encoded string by 1 *)
 
 val ulid : ?seed_time:int -> unit -> string
-val ulid_factory : ?prng:(int -> int) -> unit -> (?seed_time:int -> unit -> string)
-val monotonic_factory : ?prng:(int -> int) -> unit -> (?seed_time:int -> unit -> string)
+(** Returns a randomly generated ULID using the current time or an optional seed time *)
+
+val ulid_factory :
+  ?prng:(int -> int) -> unit -> ?seed_time:int -> unit -> string
+(** Returns a function to generate ULIDs using an optionally specified PRNG *)
+
+val monotonic_factory :
+  ?prng:(int -> int) -> unit -> ?seed_time:int -> unit -> string
+(** Returns a function to generate monotonic ULIDs using an optionally specified PRNG *)
